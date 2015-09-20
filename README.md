dc92a50a460858754177e2a88a579f9d0fa616bd
animack <ani.mack@gmail.com>
2015-09-20 14:34:34 +0200

    css autoprefixer

---
+++ README.md
@@ -0,0 +1,18 @@
+# css-autoprefixer
+
+css-autoprefixer is an extension for the code editor CodeTasty that adds prefixes to css files upon saving and can be also used as plugin for less/scss compiler.
+
+
+###e Options
+
+Options can be set in the first line of the edited file:
+
+    /* plugin: css-autoprefixer, out: style.prefixed.css */
+
+out: prefixed file destination
+
+    /* out: ., app.css, ../style.css */
+
+plugin: enables autoprefixer, can be used in less/css compiler options
+
+    /* plugin: css-autoprefixer */
---
+++ extension.js
@@ -0,0 +1,69 @@
+define(function(require, exports, module) {
+	var ExtensionManager = require('code/extensionManager');
+	
+	var Code = require('code/code');
+	var Socket = require('code/socket');
+	var Workspace = require('code/workspace');
+	var Notification = require('code/notification');
+	var Fn = require('code/fn');
+	var FileManager = require('code/fileManager');
+	
+	var Autoprefixer = require('./autoprefixer');
+	
+	var EditorSession = require('modules/editor/ext/session');
+	
+	var Extension = ExtensionManager.register({
+		name: 'css-autoprefixer',
+		
+	}, {
+		init: function() {
+			var self = this;
+			EditorSession.on('save', function(e) {
+				if (self._exts.indexOf(e.storage.extension) !== -1) {
+					Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.getValue());
+				}
+			});
+		},
+		_exts: ['css'],
+		plugin: function(css, cb) {
+			Autoprefixer.process(css, { add: false, browsers: [] }).then(function(cleaned) {
+				Autoprefixer.process(cleaned.css).then(function(prefixed) {
+					cb(prefixed.css);
+				}, function(error) {
+					cb(null, error);
+				});
+			}, function(error) {
+				cb(null, error);
+			});
+		},
+		compile: function(workspaceId, path, doc) {
+			var self = this;
+			var options = FileManager.getFileOptions(doc, /^\s*\/\*\s*(.+)\*\//);
+			
+			if (!options.out || options.plugin != "css-autoprefixer") {
+				return false;
+			}
+			
+			var destination = FileManager.parsePath(path, options.out, [this._exts.join('|'), 'css']);
+			
+			if (!destination) {
+				return false;
+			}
+			
+			this.plugin(doc.substring(doc.indexOf("\n") + 1), function(output, error) {
+				if (error) {
+					Notification.open({
+						type: 'error',
+						title: 'Autoprefix failed.',
+						description: error.message + ' on ' + error.line + ':' + error.column
+					});
+					return false;
+				}
+				
+				FileManager.saveFile(workspaceId, destination, output, null);
+			});
+		}
+	});
+
+	module.exports = Extension;
+});
